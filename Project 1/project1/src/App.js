511c9b4d148d2f8138bc41565fa324ea6ff5eeda
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2022-11-29 09:49:34 +0530

    commit

---
+++ Project 1/project1/src/App.js
@@ -0,0 +1,13 @@
+
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
