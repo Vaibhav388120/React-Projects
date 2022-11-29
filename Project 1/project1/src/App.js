4ff8065135f8e196e920d079d641381559badbcf
Vaibhav29092000 <vaibhavachegave29092000@gmail.com>
2022-11-29 11:12:44 +0530

    commit

--- Project 1/project1/src/App.js
+++ Project 1/project1/src/App.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses";
-function App() {
+const App=()=> {
   const expenses=[{
     id:'e1',
     title:'Toilet Paper',
@@ -26,6 +26,17 @@ function App() {
     date: new Date(2021,5,12),
   },
   ];
+
+/*
+return React.createElement(
+  'div',
+  {},
+  React.createElement('h2',{},"Let's get started"),
+  React.createElement(Expenses,{items:expenses})
+)
+*/
+
+
   return (
     <div>
       <Expenses items={expenses}/>
--- Project 1/project1/src/components/ExpenseItem.js
+++ Project 1/project1/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-function ExpenseItem(props){
+import Card from './Card';
+const ExpenseItem=(props)=>{
 
 
     return(
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
             </div>
             <div className='expense-item__price'>${props.amount}
             </div>
-        </div>
+        </Card>
     )
 }
 
